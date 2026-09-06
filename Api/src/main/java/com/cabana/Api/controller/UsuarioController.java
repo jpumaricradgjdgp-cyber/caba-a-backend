ae9c0fd63eaf11e62cdd825238fccd275967ce0b
Josue Gp <jpumaricradg.jdgp@gmail.com>
2026-09-06 13:51:01 -0500

    se corrigio controllers

--- Api/src/main/java/com/cabana/Api/controller/UsuarioController.java
+++ Api/src/main/java/com/cabana/Api/controller/UsuarioController.java
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+@CrossOrigin(origins = {"http://127.0.0.1:5500", "http://localhost:5500"})
 @RestController
 @RequestMapping("/api/usuarios")
 public class UsuarioController {
